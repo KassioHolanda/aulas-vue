ef33efb7136517ba48e5553d003c9c1b4c3d6ad3
KassioHolanda <holandakassio@hotmail.com>
2021-02-23 20:21:21 -0300

    desafio 2

---
+++ dom/desafio-02/app.js
@@ -0,0 +1,14 @@
+new Vue({
+
+    el:'#desafio',
+    data: {
+        valor: ''
+    },
+    methods: {
+        exibirAlerta() {
+            alert('Estou ti alertando agora')
+        }
+
+    }
+
+})
