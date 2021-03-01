bab64cf4c8ac9efef2563fd4ff270c70381d526b
Kassio Holanda <holandakassio@hotmail.com>
2021-03-01 15:51:28 -0300

    implementing attack actions

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -15,7 +15,27 @@ new Vue({
             this.running = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+        },
+
+        attack(special) { 
+            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, special)
+        },
+
+        hurt(nameLife, min, max, special) {
+            const plus = special ? 5 : 0
+            const hurt = this.getRadom(min + plus, max + plus)
+            this[nameLife] = Math.max(this[nameLife] - hurt, 0)
+        },
+
+        getRadom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value);
         }
     },
-    watch: {}
+    watch: {
+        hasResult(value) {
+            if (value) this.running = false
+        }
+    }
 })
